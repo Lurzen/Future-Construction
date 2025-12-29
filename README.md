d3d874dfad06b67f7d4e68574bcbbd73a4bbe9c0
Ali Ali <ali.yksl7@gmail.com>
2025-12-29 19:49:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Preview
+
+<img src="./docs/gifs/preview.gif"/>
+
+## Header
+
+<img src="./docs/screenshots/header.png"/>
+
+## Poster
+
+<img src="./docs/screenshots/poster.png"/>
+
+## Stats
+
+<img src="./docs/screenshots/stats.png"/>
+
+## Projects
+
+<img src="./docs/screenshots/projects.png"/>
+
+## Company
+
+<img src="./docs/screenshots/company.png"/>
+
+## Footer
+
+<img src="./docs/screenshots/footer.png"/>
---
+++ js/app.js
@@ -0,0 +1,67 @@
+// Display Menu
+function displayMenu() {
+  var link = document.getElementById("main-nav");
+  link.classList.toggle("display");
+}
+
+// Projects Slider
+document.addEventListener("DOMContentLoaded", function () {
+  var main = new Splide("#main-slider", {
+    type: "fade",
+    pagination: false,
+    arrows: false,
+    drag: false,
+  });
+
+  var text = new Splide("#text-slider", {
+    type: "fade",
+    pagination: false,
+    arrows: false,
+    drag: false,
+  });
+
+  var thumbnails = new Splide("#thumbnail-slider", {
+    fixedWidth: false,
+    perPage: 1.5,
+    gap: 15,
+    pagination: false,
+    arrows: false,
+    isNavigation: true,
+    focus: 0,
+  });
+
+  main.sync(text);
+  main.sync(thumbnails);
+
+  main.mount();
+  text.mount();
+  thumbnails.mount();
+
+  var nextBtn = document.querySelector(".js-next");
+  var prevBtn = document.querySelector(".js-prev");
+
+  if (nextBtn && prevBtn) {
+    nextBtn.addEventListener("click", () => main.go(">"));
+    prevBtn.addEventListener("click", () => main.go("<"));
+  }
+});
+
+// News Slider
+document.addEventListener("DOMContentLoaded", function () {
+  var newsSplide = new Splide("#news-slider", {
+    type: "loop",
+    perPage: 2.5,
+    perMove: 1,
+    gap: "4rem",
+    padding: "2rem",
+
+    breakpoints: {
+      1024: {
+        perPage: 1,
+      },
+    },
+    focus: 0,
+    omitEnd: true,
+  });
+  newsSplide.mount();
+});
